4f9c75c749a1c7f888daf7c13ae907d7aec0a6a3
betty2000 <a0935030154@gmail.com>
2018-12-11 12:01:29 +0800

    unfinished

---
+++ src/com/exam/Order.java
@@ -0,0 +1,21 @@
+package com.exam;
+
+public class Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void add(Ticket ticket) {
+		Ticket t = new icket; 
+		
+	}
+
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
---
+++ src/com/exam/Ticket.java
@@ -0,0 +1,17 @@
+package com.exam;
+
+public class Ticket {
+
+	public Ticket(String string, String string2, int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Taipei");
+		System.out.print("Keelung");
+		System.out.print("37");
+		System.out.print("2");
+	}
+
+}
